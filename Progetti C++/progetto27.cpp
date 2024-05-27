1a2747ac0cccb5d84b78d27f075c2e35631eb497
Samuele Zanin <samueleudine@gmail.com>
2024-05-27 08:36:57 +0000

    Prove in classe

---
+++ Progetti C++/progetto27.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Benvenuto su Gitpod!"<<endl;
+    string name;
+    cout<<"Scrivi il tuo nome: "<<endl;
+    cin>>name;
+    cout<<"Ciao!"<<endl;
+
+    return 0;
+}
